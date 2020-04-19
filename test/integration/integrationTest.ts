e65d0bb27326246eae64562195a992f1e1c81618
Simon Doba <simon.doba@hotmail.de>
2020-04-19 18:10:43 +0200

    Added Unit & Testintegration example

---
+++ test/integration/integrationTest.ts
@@ -0,0 +1,22 @@
+import container from "../../config/inversify.config";
+import {TYPES} from "../../config/types";
+import { Client } from "discord.js";
+import { Bot } from "../../app/Feature/Bot";
+// ...
+
+describe('Bot', () => {
+  let discordMock: Client;
+  let discordInstance: Client;
+  let bot: Bot;
+
+  beforeEach(() => {
+  //  discordMock = mock(Client);
+ //   discordInstance = instance(discordMock);
+    container.rebind<Client>(TYPES.Client)
+      .toConstantValue(discordInstance);
+    bot = container.get<Bot>(TYPES.Bot);
+  });
+
+  // Test cases here
+
+});
---
+++ test/unit/MessageServiceTest.ts
@@ -0,0 +1,66 @@
+import "reflect-metadata";
+import 'mocha';
+import {expect} from 'chai';
+import {instance, mock, verify, when} from "ts-mockito";
+import {Message} from "discord.js";
+import {MessageResponder} from '../../app/Service/Chat/MessageResponder'
+import {PingTest} from '../../app/Service/Chat/pingTest'
+
+describe('MessageResponder', () => {
+  let mockedPingFinderClass: PingTest;
+  let mockedPingFinderInstance: PingTest;
+  let mockedMessageClass: Message;
+  let mockedMessageInstance: Message;
+
+  let service: MessageResponder;
+
+  beforeEach(() => {
+    mockedPingFinderClass = mock(PingFinder);
+    mockedPingFinderInstance = instance(mockedPingFinderClass);
+    mockedMessageClass = mock(Message);
+    mockedMessageInstance = instance(mockedMessageClass);
+    setMessageContents();
+
+    service = new MessageResponder(mockedPingFinderInstance);
+  })
+
+  it('should reply', async () => {
+    whenIsPingThenReturn(true);
+
+    await service.handle(mockedMessageInstance);
+
+    verify(mockedMessageClass.reply('pong!')).once();
+  })
+
+  it('should not reply', async () => {
+    whenIsPingThenReturn(false);
+
+    await service.handle(mockedMessageInstance).then(() => {
+      // Successful promise is unexpected, so we fail the test
+      expect.fail('Unexpected promise');
+    }).catch(() => {
+	 // Rejected promise is expected, so nothing happens here
+    });
+
+    verify(mockedMessageClass.reply('pong!')).never();
+  })
+
+  function setMessageContents() {
+    mockedMessageInstance.content = "Non-empty string";
+  }
+
+  function whenIsPingThenReturn(result: boolean) {
+    when(mockedPingFinderClass.isPing("Non-empty string")).thenReturn(result);
+  }
+
+  describe('PingTest', () => {
+    let service: PingTest;
+    beforeEach(() => {
+      service = new PingTest();
+    })
+  
+    it('should find "ping" in the string', () => {
+      expect(service.isPing("ping")).to.be.true
+    })
+  });
+});
